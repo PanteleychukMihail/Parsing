dae09c0148dcf5fe732bd12883619f8449da3f11
PanteleychukMihail <PanteleychukMihail@gmail.com>
2022-07-26 14:40:40 +0300

    init

---
+++ pars.py
@@ -0,0 +1,25 @@
+import requests
+
+
+def download(q: str, p: str):
+    header = {"Authorization": "563492ad6f9170000100000191531d4acb0e4a5eba1d002473d6ae53"}
+    i = 1
+    while i <= int(p):
+        url = f"https://api.pexels.com/v1/search?query={q}&per_page=1&page={i}"
+        r = requests.get(url, headers=header)
+        if r.status_code == 200:
+            _r = r.json()
+            for item in _r.get("photos"):
+                print(item.get("url"))
+        else:
+            print(r.status_code)
+        i += 1
+
+
+def main() -> None:
+    q = input("query  ")
+    p = input("count  ")
+    download(q, p)
+
+
+main()
---
+++ pars1.py
@@ -0,0 +1,32 @@
+import requests
+from PIL import Image
+from io import BytesIO
+
+
+def download(query: str, page_count: int) -> None:
+    header = {"Authorization": "563492ad6f9170000100000191531d4acb0e4a5eba1d002473d6ae53"}
+    params = {"query": query, "per_page": 1}
+    url = f"https://api.pexels.com/v1/search"
+    i = 1
+    while i <= page_count:
+        params["page"] = i
+        r = requests.get(url, headers=header, params=params)
+        if r.status_code == 200:
+            _r = r.json()
+            for item in _r.get("photos"):
+                _img_url = item.get("src").get("original")
+                resp = requests.get(_img_url)
+            image=Image.open(BytesIO(resp.content))
+            image.save(f'media/{query}_{i}.{_img_url.split(".")[-1]}')
+        else:
+            print(r.status_code)
+        i += 1
+
+
+def main() :
+    query = input("Ouery  ")
+    page_count = int(input("Count_page  "))
+    download(query, page_count)
+
+
+main()
